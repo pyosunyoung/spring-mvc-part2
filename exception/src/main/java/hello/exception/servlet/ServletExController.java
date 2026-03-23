242909c12ad7f085bdf1b9e423bde96f632821f8
pyosunyoung <tjsdudtbvj12@naver.com>
2026-03-23 17:48:57 +0900

    feat: 서블릿 기반 예외 처리 및 오류 페이지 처리, 필터/인터셉터 예외 흐름 적용

---
+++ exception/src/main/java/hello/exception/servlet/ServletExController.java
@@ -0,0 +1,27 @@
+package hello.exception.servlet;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class ServletExController {
+    @GetMapping("/error-ex")
+    public void errorEx() {
+        throw new RuntimeException("예외 발생!");//exception이 터지면 무조건 500에러
+    }
+
+    @GetMapping("/error-404")
+    public void error404(HttpServletResponse response) throws IOException { //IOException을 넣어야 snedError 가능
+        response.sendError(404, "404 오류!"); //send error는 상태코드 지정 가능.
+    }
+
+    @GetMapping("/error-500")
+    public void error500(HttpServletResponse response) throws IOException {
+        response.sendError(500); //response안에 send error 저장 가능.
+    }
+}
