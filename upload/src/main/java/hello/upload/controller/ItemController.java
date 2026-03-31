02c2951e8d43ec23e46e8f74eaf82a2fa3134230
pyosunyoung <tjsdudtbvj12@naver.com>
2026-03-31 11:20:11 +0900

    feat: 파일 다운로드 기능 구현

--- upload/src/main/java/hello/upload/controller/ItemController.java
+++ upload/src/main/java/hello/upload/controller/ItemController.java
@@ -10,15 +10,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriUtils;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Slf4j
@@ -67,8 +70,19 @@ public class ItemController {
         return new UrlResource("file:" + fileStore.getFullPath(filename));
     }
 
-//    @GetMapping("/attach/{itemId}")
-//    public ResponseEntity<Resource> downloadAttachFile(@PathVariable Long itemId) throws IOException {
-//
-//    }
+    @GetMapping("/attach/{itemId}")
+    public ResponseEntity<Resource> downloadAttachFile(@PathVariable Long itemId) throws IOException {
+        Item item = itemRepository.findById(itemId);// 이 item을 접근할 수 있는 사용자만 다운 가능 즉 itemId를 꼭 받아야함
+        String storeFileName = item.getAttachFile().getStoreFileName();
+        String uploadFileName = item.getAttachFile().getUploadFileName();
+
+        UrlResource resource = new UrlResource("file:" + fileStore.getFullPath(storeFileName));
+
+        log.info("uploadFileName={}", uploadFileName);
+        String encodedUploadFileName = UriUtils.encode(uploadFileName, StandardCharsets.UTF_8); // 인코딩
+
+        String contentDisposition = "attachment; filename=\"" + encodedUploadFileName + "\""; //DISPOSITION 설정해야
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition).body(resource);
+
+    }
 }
